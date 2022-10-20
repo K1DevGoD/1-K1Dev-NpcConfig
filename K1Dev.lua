fb2f77023e5733b971621c0d7dc95bd2e7d3e19c
K1Dev <devgodno1@gmail.com>
2022-10-20 20:40:25 +0700

    1-K1Dev-NpcConfig
    1-K1Dev-NpcConfig

---
+++ K1Dev.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Traffic = {
+    pedFrequency = 0, --[[ @ Npc ]]
+    trafficFrequency = 0.0, --[[  @ เกวียนหรือยานพาหนะ ]]
+    animalFrequency = 5.3, --[[  @ สัตว์ตามแมพ ]]
+}
